10b24f1e55cc7ca751a0ae6a907fa4307a2e2934
ARZUMATA <arzumata@gmail.com>
2026-03-31 11:33:52 +0300

    fix: refactor and toonshader now working

--- Apex_toolbox/operators/toon_autotex.py
+++ Apex_toolbox/operators/toon_autotex.py
@@ -62,10 +62,10 @@ class BUTTON_TOON(bpy.types.Operator):
         scene.view_settings.look = 'Medium High Contrast'
         
         # Append Black Outline material if it doesn't exist
-        mat = bpy.data.materials.get("Black Outline")
-        if mat is None:
+        mat_blackoutline = bpy.data.materials.get("Black Outline")
+        if mat_blackoutline is None:
             bpy.ops.wm.append(directory=my_path + blend_file + ap_material, filename='Black Outline')
-            mat = bpy.data.materials.get("Black Outline")
+            mat_blackoutline = bpy.data.materials.get("Black Outline")
         
         # Select all objects in selection
         for x in range(len(selection)):
@@ -83,7 +83,7 @@ class BUTTON_TOON(bpy.types.Operator):
                 
                 if isbase is False:
                     if "Black Outline" not in o.material_slots:
-                        o.data.materials.append(mat)
+                        o.data.materials.append(mat_blackoutline)
                         # Add Solidify modifier for outline
                         exists = False
                         for mod in o.modifiers:
@@ -106,65 +106,96 @@ class BUTTON_TOON(bpy.types.Operator):
 
                 for mSlot in o.material_slots:
                     MatNodeTree = bpy.data.materials[mSlot.name]
-                    try:
-                        imageNode = MatNodeTree.node_tree.nodes["Image Texture"]
-                    except:
-                        print(MatNodeTree.name)
-                        continue
-                    try:
-                        image = os.path.basename(os.path.abspath(imageNode.image.filepath))
-                    except:
-                        print(mSlot.name, "missing texture.")
-                        continue
-                    imagepath = os.path.dirname(os.path.abspath(imageNode.image.filepath))
-                    imageType = imageNode.image.name.split(".")[0].split('_')[-1]
-                    imageName = MatNodeTree.name
-                    imageFormat = image.split('.')[1]
                     
-                    if not any(imageType in x for x in texSets):
-                        print(image, "could not be mapped.")
-                        continue
-
-                    # Clear existing nodes
-                    MatNodeTree.node_tree.nodes.clear()
+                    # Define SHADER_CONFIGS for Toon Shader mapping (adapted from autotex.py)
+                    TOON_SHADER_CONFIG = {
+                        'toon': {
+                            'node_group_name': 'Apex ToonShader',
+                            'color_dict': {
+                                "Albedo": "--- Base Color ---",
+                                "Specular": "--- Specular map ---",
+                                "Emission": "--- Emission Map ---",
+                            },
+                            'alpha_dict': {},  # No alpha mapping for toon shader in this context
+                        }
+                    }
                     
-                    # Load texture images
-                    for i in range(len(texSets)):
-                        for j in range(len(texSets[i])):
-                            texImageName = imageName + '_' + texSets[i][j] + '.' + imageFormat
-                            texImage = bpy.data.images.get(texImageName)
-                            texFile = imagepath + fbs + texImageName
-                            if not texImage and loadImages:
-                                if os.path.isfile(texFile):
-                                    texImage = bpy.data.images.load(texFile)
-                            if texImage:
-                                if i > 2:
-                                    texImage.colorspace_settings.name = 'Non-Color'
-                                texImage.alpha_mode = 'CHANNEL_PACKED'
-                                texNode = MatNodeTree.node_tree.nodes.new('ShaderNodeTexImage')
-                                texNode.image = texImage
-                                texNode.name = str(i)
-                                texNode.location = (-50, 50 - 260 * i)
-                                break
+                    config = TOON_SHADER_CONFIG['toon']
+                    node_group_name = config['node_group_name']
                     
-                    # Apply Toon Shader based on material type
+                    # Apply Toon Shader based on material type (reuse existing image nodes, create fresh output)
+                    
+                    # Remove old shader nodes but keep image texture nodes
+                    # Using list() to avoid modification during iteration issues
+                    for node in list(MatNodeTree.node_tree.nodes):
+                        should_remove = False
+                        
+                        # Check if it's a BSDF/SHADER type
+                        if node.type == 'BSDF':
+                            should_remove = True
+                        
+                        # Check for common old shader group names - remove them
+                        elif node.type == 'GROUP' and node.node_tree:
+                            if node.node_tree.name in ['Apex Shader', 'Apex Shader+_v3.4', 'S/G-Blender']:
+                                should_remove = True
+                            # Keep only Apex ToonShader, check its output type to be sure
+                            elif node.node_tree.name == 'Apex ToonShader':
+                                # Verify it's actually a ToonShader by checking outputs
+                                is_toon_shader = False
+                                if hasattr(node.node_tree, 'outputs') and len(node.node_tree.outputs) > 0:
+                                    for out in node.node_tree.outputs:
+                                        if out.name == 'Shader' or out.type == 'SHADER':
+                                            is_toon_shader = True
+                                            break
+                                # If it's a ToonShader, keep it, otherwise remove
+                                if not is_toon_shader:
+                                    should_remove = True
+                            else:
+                                # Unknown group type - keep it
+                                print("Unknown group type {1} for {2}. Keeping.", node.type, mSlot.name)
+                                should_remove = False
+                        
+                        # Remove ALL output nodes (type is OUTPUT_MATERIAL) from previous setup
+                        elif node.type == 'OUTPUT_MATERIAL':
+                            should_remove = True
+                        elif node.type == 'MIX_SHADER' and 'Mix' in node.name:
+                            should_remove = True
+                        elif node.type == 'BSDF_TRANSPARENT':
+                            should_remove = True
+                        
+                        if should_remove:
+                            MatNodeTree.node_tree.nodes.remove(node)
+                    
+                    # Create fresh Material Output node as needed
+                    NodeOutput = None
+                    
                     if isbase:
                         if mSlot.name == 'wraith_base_eyecornea' or mSlot.name == 'wraith_base_eyeshadow':
+                            # Create fresh Material Output node
                             NodeOutput = MatNodeTree.node_tree.nodes.new('ShaderNodeOutputMaterial')
                             NodeOutput.location = (800, 0)
                             node_transparency = MatNodeTree.node_tree.nodes.new(type="ShaderNodeBsdfTransparent")
-                            node_transparency.location = 300, 200 
+                            node_transparency.location = 300, 200
                             MatNodeTree.node_tree.links.new(NodeOutput.inputs[0], node_transparency.outputs[0])
                         else:
+                            # Create Toon Shader node group
                             NodeGroup = MatNodeTree.node_tree.nodes.new('ShaderNodeGroup')
-                            NodeGroup.node_tree = bpy.data.node_groups.get('Apex ToonShader')
-                            if 'Group' in MatNodeTree.node_tree.nodes:
-                                MatNodeTree.node_tree.nodes['Group'].name = 'Apex ToonShader'
-                            MatNodeTree.node_tree.nodes['Apex ToonShader'].label = 'Apex ToonShader'
-                            NodeGroup.location = (300, 0)
-                            NodeOutput = MatNodeTree.node_tree.nodes.new('ShaderNodeOutputMaterial')
-                            NodeOutput.location = (800, 0)
+                            NodeGroup.node_tree = bpy.data.node_groups.get(node_group_name)
                             
+                            # Rename if duplicate exists
+                            if 'Group' in MatNodeTree.node_tree.nodes:
+                                MatNodeTree.node_tree.nodes['Group'].name = node_group_name
+                            if node_group_name in MatNodeTree.node_tree.nodes:
+                                MatNodeTree.node_tree.nodes[node_group_name].label = node_group_name
+                            
+                            NodeGroup.location = (300, 0)
+                            
+                            # Reuse existing output node or create new one
+                            if NodeOutput is None:
+                                NodeOutput = MatNodeTree.node_tree.nodes.new('ShaderNodeOutputMaterial')
+                                NodeOutput.location = (800, 0)
+                            
+                            # Setup transparency mix for base materials
                             node_transparency = MatNodeTree.node_tree.nodes.new(type="ShaderNodeBsdfTransparent")
                             node_transparency.location = 300, 200
                             node_mix = MatNodeTree.node_tree.nodes.new(type="ShaderNodeMixShader")
@@ -173,41 +204,55 @@ class BUTTON_TOON(bpy.types.Operator):
                             MatNodeTree.node_tree.links.new(node_mix.inputs[1], node_transparency.outputs[0])
                             MatNodeTree.node_tree.links.new(node_mix.inputs[2], NodeGroup.outputs[0])
                             MatNodeTree.node_tree.links.new(node_mix.outputs[0], NodeOutput.inputs[0])
-                            if mSlot.name == 'wraith_base_eye':
-                                node_mix.inputs[0].default_value = 1 
                             
-                        try:
-                            MatNodeTree.node_tree.links.new(NodeGroup.inputs["--- Base Color ---"], MatNodeTree.node_tree.nodes[0].outputs["Color"])
-                        except:
-                            pass
-                        
-                        if mSlot.name == 'wraith_base_hair':
-                            try:
-                                MatNodeTree.node_tree.links.new(node_mix.inputs[0], MatNodeTree.node_tree.nodes[3].outputs["Color"])
-                            except:
-                                pass
-
+                            if mSlot.name == 'wraith_base_eye':
+                                node_mix.inputs[0].default_value = 1
+                            
+                            # Link existing texture nodes to Toon Shader inputs (adapted from autotex.py)
+                            for slot_name, input_name in config['color_dict'].items():
+                                try:
+                                    tex_node = MatNodeTree.node_tree.nodes[slot_name]
+                                    if hasattr(tex_node, 'image') and tex_node.image:
+                                        MatNodeTree.node_tree.links.new(NodeGroup.inputs[input_name], tex_node.outputs["Color"])
+                                except KeyError:
+                                    # Texture node not found for this slot, skip
+                                    pass
+                            
+                            # Special case for wraith_base_hair
+                            if mSlot.name == 'wraith_base_hair':
+                                try:
+                                    hair_tex = MatNodeTree.node_tree.nodes['3']
+                                    MatNodeTree.node_tree.links.new(node_mix.inputs[0], hair_tex.outputs["Color"])
+                                except KeyError:
+                                    pass
+                    
                     else:
+                        # Non-base material - apply Toon Shader with texture remapping (create fresh output node)
                         NodeGroup = MatNodeTree.node_tree.nodes.new('ShaderNodeGroup')
-                        NodeGroup.node_tree = bpy.data.node_groups.get('Apex ToonShader')
+                        NodeGroup.node_tree = bpy.data.node_groups.get(node_group_name)
+                        
                         if 'Group' in MatNodeTree.node_tree.nodes:
-                            MatNodeTree.node_tree.nodes['Group'].name = 'Apex ToonShader'
-                        MatNodeTree.node_tree.nodes['Apex ToonShader'].label = 'Apex ToonShader'
+                            MatNodeTree.node_tree.nodes['Group'].name = node_group_name
+                        if node_group_name in MatNodeTree.node_tree.nodes:
+                            MatNodeTree.node_tree.nodes[node_group_name].label = node_group_name
+                        
                         NodeGroup.location = (300, 0)
+                        
+                        # Create fresh Material Output node
                         NodeOutput = MatNodeTree.node_tree.nodes.new('ShaderNodeOutputMaterial')
                         NodeOutput.location = (500, 0)
+                        
+                        # Link Node Group Output to Material Output
                         MatNodeTree.node_tree.links.new(NodeOutput.inputs[0], NodeGroup.outputs[0])
-                         
-                        ColorDict = {
-                            "0": "--- Base Color ---",
-                            "1": "--- Specular map ---",
-                            "2": "--- Emission Map ---",
-                        }
-                                           
-                        for slot in ColorDict:
+                        
+                        # Link existing texture nodes to Toon Shader inputs (adapted from autotex.py)
+                        for slot_name, input_name in config['color_dict'].items():
                             try:
-                                MatNodeTree.node_tree.links.new(NodeGroup.inputs[ColorDict[slot]], MatNodeTree.node_tree.nodes[slot].outputs["Color"])
-                            except:
+                                tex_node = MatNodeTree.node_tree.nodes[slot_name]
+                                if hasattr(tex_node, 'image') and tex_node.image:
+                                    MatNodeTree.node_tree.links.new(NodeGroup.inputs[input_name], tex_node.outputs["Color"])
+                            except KeyError:
+                                # Texture node not found for this slot, skip
                                 pass
                     
                     mSlot.material.blend_method = 'HASHED'
